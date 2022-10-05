341ba120ac956b47c20674146a56a63211d49c29
Priyanka6276 <priyankachanbrasegaran@gmail.com>
2022-10-05 18:22:31 -0400

    Problem 1

---
+++ functions-hw.js
@@ -0,0 +1,18 @@
+// 1.
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+  }
+  
+  console.log(maxOfTwoNumbers(3, 9));
+  
+  //__________________________________________________________________________
+
+  // 2.
+  
