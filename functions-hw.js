e3f347e5ac64597e6cc6384bcadf59f268a127ef
Priyanka6276 <priyankachanbrasegaran@gmail.com>
2022-10-05 19:14:59 -0400

    Third Problem

--- functions-hw.js
+++ functions-hw.js
@@ -14,18 +14,29 @@
   
   //__________________________________________________________________________
 
-  // 2.
-  const maxOfThree = function (num1, num2, num3){
-    if (num1 >= num2 && num1 >= num3){
-        return num1
-    } else if (num2 >= num1 && num2 >= num3){
-        return num2
-    }else {
-        return num3
-    }
-  }
-  console.log(maxOfThree(5,4,3))
+//   // 2.
+//   const maxOfThree = function (num1, num2, num3){
+//     if (num1 >= num2 && num1 >= num3){
+//         return num1
+//     } else if (num2 >= num1 && num2 >= num3){
+//         return num2
+//     }else {
+//         return num3
+//     }
+//   }
+//   console.log(maxOfThree(5,4,3))
 
 //_____________________________________________________________________________
 
 // 3.
+function isCharaAVowel (letter) {
+    const vowel = ["a", "e", "i", "o", "u"] 
+    for (let i = 0; i <= vowel.length; i++) {
+        if (letter === vowel[i]){
+            return true
+        }         
+    }
+    return false
+}
+
+console.log(isCharaAVowel("f"))
